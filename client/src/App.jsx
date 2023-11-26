d9e9e2a3ea03e2715ce4f65fb2e19eaafd17d4ec
AbcdWxyz <150275116+saikishan9@users.noreply.github.com>
2023-11-26 13:45:49 +0530

    Added Routes and create Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, About, Profile, SignIn, SignUp } from "./pages/index";
+
 export default function App() {
-  return <div>App</div>;
+  const paths = [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    { path: "/about", element: <About /> },
+    { path: "/profile", element: <Profile /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/sign-up", element: <SignUp /> },
+  ];
+
+  return (
+    <Router>
+      <Routes>
+        {paths.map(({ path, element }) => (
+          <Route key={path} path={path} element={element}></Route>
+        ))}
+      </Routes>
+    </Router>
+  );
 }
