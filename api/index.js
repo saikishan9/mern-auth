1b420159b552ab4b70c517fe912a20465b2f582a
AbcdWxyz <150275116+saikishan9@users.noreply.github.com>
2023-11-26 15:07:03 +0530

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log(`Connected to MongoDB`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
