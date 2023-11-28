8fde00c5d4582e1f6f78b0b7b4b19901158038d4
AbcdWxyz <150275116+saikishan9@users.noreply.github.com>
2023-11-28 10:13:58 +0530

    Added middleware & customFunction to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashesPassword = bcryptjs.hashSync(password);
   const newUser = new User({ username, email, password: hashesPassword });
@@ -9,6 +9,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: `User created successfully` });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
+    // next(errorHandler(300, "Something went wrong"));
   }
 };
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -24,3 +25,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
