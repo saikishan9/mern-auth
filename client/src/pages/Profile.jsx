9a93e6f9cca43fe8f2352b124ad8162f26d059c1
AbcdWxyz <150275116+saikishan9@users.noreply.github.com>
2023-12-03 11:37:23 +0530

    Update Image Upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,60 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
   const [formData, setFormData] = useState({});
+  const [image, setImage] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(0);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const fileRef = useRef(null);
+
+  useEffect(() => {
+    if (image) {
+      handleImageUpload(image);
+    }
+  }, [image]);
+
+  const handleImageUpload = (image) => {
+    try {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + image.name;
+
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, image);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(Math.round(progress));
+        },
+        (error) => {
+          console.log(error);
+          setImageUploadError(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref)
+            .then((downloadURL) => {
+              setFormData({ ...formData, profilePhoto: downloadURL });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleChange = (e) => {
     const {
@@ -56,11 +107,34 @@ export default function Profile() {
         onSubmit={handleSubmit}
         className="flex flex-col justify-center gap-4"
       >
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileRef}
+          hidden
+          onChange={(e) => setImage(e.target.files[0])}
+        />
         <img
           src={currentUser.profilePhoto}
           alt="profilePhoto"
           className="h-24 w-24 self-center rounded-full object-cover cursor-pointer mt-2"
+          onClick={() => fileRef.current?.click()}
         />
+
+        <p className="text-sm self-center">
+          {imageUploadError ? (
+            <span className="text-red-700">
+              Error Uploading Image (file size must be less than 6MB)
+            </span>
+          ) : imageUploadProgress > 0 && imageUploadProgress < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${imageUploadProgress}%...`}</span>
+          ) : imageUploadProgress === 100 ? (
+            <span className="text-green-700">Image Uploaded Successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
+
         <input
           type="text"
           placeholder="Username"
